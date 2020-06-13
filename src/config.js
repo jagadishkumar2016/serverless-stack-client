ca0a941726445c7ed00b4b18d3fe907dd8c46081
jagadish2090 <jagadish@cocolevio.com>
2020-06-13 18:22:28 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "notes-app-uploads-jaga",
+    BUCKET: "us-east-1"
+  },
+  apiGateway: {
+    REGION: "https://hgpovcrq4b.execute-api.us-east-1.amazonaws.com/prod",
+    URL: "us-east-1"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_YslEaqWE8",
+    APP_CLIENT_ID: "sa0gkml03efan17iabu5dq4dt",
+    IDENTITY_POOL_ID: "us-east-1:faf1ab92-1206-4254-9292-8e4f5d491cb4"
+  }
+};
